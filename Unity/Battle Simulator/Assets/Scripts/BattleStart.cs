4a977bc412d6802c86c04f238e5c8b69db4e29d3
zdravko7 <zdravko7@abv.bg>
2015-10-05 13:06:08 +0300

    Added Battle Simulator Project

---
+++ Unity/Battle Simulator/Assets/Scripts/BattleStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine(Wait());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        Application.LoadLevel(2);
+    }
+}
---
+++ Unity/Battle Simulator/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+
+    public Canvas quitMenu;
+    public Button play;
+    public Button exit;
+
+    // Use this for initialization
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        play = play.GetComponent<Button>();
+        exit = exit.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        play.enabled = false;
+        exit.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        play.enabled = true;
+        exit.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
